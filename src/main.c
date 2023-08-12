83fafc8697c31df5706ef68262de618b5ba0234b
Yehonatan Kadosh <jacojacoj3@gmail.com>
2023-08-13 01:08:20 +0300

    updated TODO

--- src/main.c
+++ src/main.c
@@ -14,41 +14,48 @@ int main(int argc, char *argv[])
     Symbols symbols;   /* Structure to store different symbols found in the file */
     Commands commands; /* Structure to store the parsed commands from the file */
 
-    /* Run the script */
+    /* Check if filename is provided */
     if (argc == 1)
     {
         logError("No filename entered :(");
         exit(-1);
     }
 
+    /* Process each provided filename */
     for (i = 1; i < argc; ++i)
     {
+        /* Validate the file */
         file = validateFile(argv[i]);
         if (file != NULL)
         {
             commands.amount = 0;
             symbols.amount = 0;
 
-            /* START OF FILE USAGE */
+            /* Remove macros and process the file */
             file = stripMacros(file, argv[i]);
 
+            /* Extract symbols and commands from the file */
             destructureFile(file, &symbols, &commands);
 
             fclose(file);
 
-            /* 1. Do Not Create the files if there is INVALID command or symbol and return */
+            /* Check for invalid commands or symbols */
             if (has_invalid_command(commands) || has_invalid_symbol(symbols))
             {
+                /* Clean up and skip to the next file */
                 free_symbols(&symbols);
                 free_commands(&commands);
                 continue;
             }
 
-            /* 2. For each command */
-            /* header of base64 will be (amount of command lines, amount of variable lines) */
-            /* write command from binary to base64 file */
-            /* write to externs usage file */
+            /* TODO: Process each command */
+
             /* write the entries to the entries file */
+
+            /* write command from binary to base64 file */
+            /* header of base64 will be (amount of command lines, amount of variable lines) */
+
+            /* write to externs usage file */
 
             /* Free memory associated with Symbols and Commands */
             free_symbols(&symbols);
