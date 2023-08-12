56f12d2d24d2f3f70e6e1b146ef24b69d4a88298
Yehonatan Kadosh <jacojacoj3@gmail.com>
2023-08-12 12:28:30 +0300

    Added free commands and symbols

--- src/main.c
+++ src/main.c
@@ -5,11 +5,14 @@
 #include "logger/logger.h"
 #include "algorithem/logic/logic.h"
 #include "algorithem/command/command.h"
+#include "algorithem/symbol/symbol.h"
 
 int main(int argc, char *argv[])
 {
     int i;
     FILE *file;
+    Symbols symbols;   /* Structure to store different symbols found in the file */
+    Commands commands; /* Structure to store the parsed commands from the file */
 
     /* Run the script */
     if (argc == 1)
@@ -23,10 +26,29 @@ int main(int argc, char *argv[])
         file = validateFile(argv[i]);
         if (file != NULL)
         {
+            commands.amount = 0;
+            symbols.amount = 0;
+
             /* START OF FILE USAGE */
             file = stripMacros(file, argv[i]);
-            destructureFile(file);
+
+            destructureFile(file, &symbols, &commands);
+
             fclose(file);
+
+            /* 1. Do Not Create the files if there is INVALID command or symbol and return */
+            if (has_invalid_command(commands) || has_invalid_symbol(symbols))
+                return;
+
+            /* 2. For each command */
+            /* header of base64 will be (amount of command lines, amount of variable lines) */
+            /* write command from binary to base64 file */
+            /* write to externs usage file */
+            /* write the entries to the entries file */
+
+            /* Free memory associated with Symbols and Commands */
+            free_symbols(&symbols);
+            free_commands(&commands);
         }
     }
 
